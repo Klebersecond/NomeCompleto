8f46541e0d9408344ace76e4da1d2387dbf8afa4
Kleber Neves <klebernssii@gmail.com>
2025-03-24 10:00:19 -0300

    Programa nome completo

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string nome, sobrenome;
+
+
+Console.WriteLine("Digite seu nome:");
+nome = Console.ReadLine();
+
+Console.WriteLine("Digite seu sobrenome:");
+sobrenome = Console.ReadLine();
+
+Console.WriteLine($"{nome} {sobrenome}");
